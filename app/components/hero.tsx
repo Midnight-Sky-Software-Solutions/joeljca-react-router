849c453d88ca1fcf1eea92cedbf5a1cb52f7ac1b
Alex Johnston <me@alexj.ca>
2025-03-17 19:36:59 -0600

    Colors!

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -3,9 +3,9 @@ export default function Hero() {
     <div className="flex justify-center bg-gray-600">
       <div className="max-w-6xl grow flex gap-6">
         <div className="w-full flex flex-col justify-center gap-8">
-          <h2 className="text-white text-4xl font-serif">Hi, I'm Joel</h2>
+          <h2 className="text-white text-4xl font-serif">Hi, I'm <span className="text-teal-300">Joel</span></h2>
           <p className="text-white text-lg">
-            I’m a Full Stack Software Developer with a passion for building robust business applications with React, ASP.NET Web API and SQL Server.
+            I’m a <span className="text-teal-300 font-bold">Full Stack Software Developer</span> with a passion for building robust business applications with React, ASP.NET Web API and SQL Server.
           </p>
         </div>
         <div className="w-full bg-[url(/its-me-2.webp)] h-120 bg-cover bg-center">
