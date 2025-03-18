de6f4c3bad3e7766c866afb04b472dee653eba5b
Alex Johnston <me@alexj.ca>
2025-03-17 18:18:23 -0600

    Work on layout

---
+++ app/components/header.tsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router";
+
+const navItems = [
+  { name: 'About Me', to: '#', external: false },
+  { name: 'Portfolio', to: '#', external: false },
+  { name: 'Blog', to: '#', external: false },
+  { name: 'Source', to: '#', external: true },
+];
+
+export default function Header() {
+  return (
+    <div className="flex justify-center shadow-md">
+      <div className="max-w-6xl grow flex gap-10">
+        <Link to="#" className="flex items-center font-bold text-lg">
+          Joel Alexander Johnston
+        </Link>
+        <nav>
+          <ul className="flex gap-10 py-5 text-lg">
+            {navItems.map(item => (
+              <li>{item.name}</li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
