5bc86e0cafee16c1c84358a60de9211709c34a56
Alex Johnston <me@alexj.ca>
2025-03-17 19:39:54 -0600

    More colors!

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,6 +1,6 @@
 export default function Hero() {
   return (
-    <div className="flex justify-center bg-gray-600">
+    <div className="flex justify-center bg-blue-grey-800">
       <div className="max-w-6xl grow flex gap-6">
         <div className="w-full flex flex-col justify-center gap-8">
           <h2 className="text-white text-4xl font-serif">Hi, I'm <span className="text-teal-300">Joel</span></h2>
